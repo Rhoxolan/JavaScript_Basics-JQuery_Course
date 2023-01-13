1248dff822599ef4213dceabdc81b6ae093dde74
Rhoxolan <pba17mail@gmail.com>
2023-01-13 21:01:18 +0200

    The third practical work was began

---
+++ 2022.12.05_PW/objectsPracticalWork.js
@@ -0,0 +1,32 @@
+function Point(x,y) { //Пример создания функции-конструктора
+    this.x = x;
+    this.y = y;
+}
+
+let rectangle = { //Пример создания объекта
+    left_top: new Point(40, 20),
+    right_bottom: new Point(350, 570),
+    toString(){ //Пример переопрделения метода toString
+        return `${this.left_top.x} ${this.left_top.y} - ${this.right_bottom.x} ${this.right_bottom.y}` //Пример использования this
+    }
+};
+rectangle.name = "rectA"; //Пример определения свойства в объекте после его создания
+
+//Task 1
+console.log(ShowRectangleInfo(rectangle));
+
+//Task 2
+
+
+
+//Попробовать потом заменить на модули
+
+function ShowRectangleInfo(r){
+    return `${r.name}: ${r}`
+}
+
+function ShowRectangleWidth(r){
+    // return     
+}
+
+
