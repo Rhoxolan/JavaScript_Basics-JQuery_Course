7aeb25074a049a77aea6faf14f5b8b0148b07269
Rhoxolan <pba17mail@gmail.com>
2023-01-14 10:10:50 +0200

    The third practical work was supplemented

--- 2022.12.05_PW/objectsPracticalWork.js
+++ 2022.12.05_PW/objectsPracticalWork.js
@@ -31,6 +31,31 @@ console.log(ShowRectangleP(rectangle));
 EditRectangleWidth(rectangle, 50);
 console.log(ShowRectangleWidth(rectangle));
 
+//Task 7
+EditRectangleHeight(rectangle, 70);
+console.log(ShowRectangleHeight(rectangle));
+
+//Task 8
+EditRectangleWidthHeight(rectangle, 15, 35);
+console.log(ShowRectangleWidth(rectangle));
+console.log(ShowRectangleHeight(rectangle));
+
+//Task 9
+MoveAlongX(rectangle, 100);
+console.log(ShowRectangleInfo(rectangle));
+
+//Task 10
+MoveAlongY(rectangle, 45);
+console.log(ShowRectangleInfo(rectangle));
+
+//Task 11
+MoveAlongXY(rectangle, 3, 57);
+console.log(ShowRectangleInfo(rectangle));
+
+//Task 12
+console.log(IsPointIsCenter(rectangle, 75, 118));
+console.log(IsPointIsCenter(rectangle, 343.25, 441.75));
+
 
 //Попробовать потом заменить на модули
 
@@ -57,4 +82,34 @@ function ShowRectangleP(r) {
 function EditRectangleWidth(r, p_count) {
     r.left_top.y -= p_count / 2;
     r.right_bottom.y += p_count / 2;
+}
+
+function EditRectangleHeight(r, p_count) {
+    r.left_top.x -= p_count / 2;
+    r.right_bottom.x += p_count / 2;
+}
+
+function EditRectangleWidthHeight(r, w_p_count, h_p_count) {
+    EditRectangleWidth(r, w_p_count);
+    EditRectangleHeight(r, h_p_count);
+}
+
+function MoveAlongX(r, p_count) {
+    r.left_top.x += p_count;
+    r.right_bottom.x += p_count;
+}
+
+function MoveAlongY(r, p_count) {
+    r.left_top.y += p_count;
+    r.right_bottom.y += p_count;
+}
+
+function MoveAlongXY(r, x_p_count, y_p_count) {
+    MoveAlongX(r, x_p_count);
+    MoveAlongY(r, y_p_count);
+}
+
+function IsPointIsCenter(r, px, py) {
+    return px == r.left_top.x + r.right_bottom.x / 2 &&
+        py == r.left_top.y + r.right_bottom.y / 2;
 }
