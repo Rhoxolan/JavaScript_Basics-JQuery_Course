9e1b745ee4baab795a320916f25ed47e7d856782
Rhoxolan <pba17mail@gmail.com>
2023-01-16 20:03:05 +0200

    The fifth practical work was supplemented

--- 2022.12.09_PW/StringsPracticalWork.js
+++ 2022.12.09_PW/StringsPracticalWork.js
@@ -1,3 +1,5 @@
+const spamWords = ["безкоштовно", "збільшення", "продажів", "тільки сьогодні", "не видаляйте", "ххх"];
+
 //Task 1.
 const str1 = "str_1"
 const str2 = new String("str2");
@@ -20,16 +22,33 @@ console.log(vowelsCount("String Of Strings"));
 console.log(vowelsCount("String\nOf\nStrings"));
 
 //Task 4.
+console.log(spamDetector("Сообщение"));
+console.log(spamDetector("Тільки сьогодні! Безкоштовно! 2 за ціною 3-х!"));
 
+//Task 5.
+console.log(TruncateString("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta ac.", 75));
+console.log(TruncateString("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta ac.", 35));
+
+//Task 6.
+console.log(isPalindrom("ABCCBA"));
+console.log(isPalindrom("ABCsCBA"));
+console.log(isPalindrom("ABCdsCBA"));
+
+//Task 7.
+console.log(wordsCount("Lorem ipsum dolor sit amet."));
+console.log(wordsCount("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut felis dapibus lacus vehicula rhoncus."));
+
+//Task 8.
+console.log(GetLongestWord("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut felis dapibus lacus vehicula rhoncus."));
 
 function getLongerString(str1, str2) {
-    if(str1.length > str2.length) { //Пример сравнения строк по длине с помошью length
+    if (str1.length > str2.length) { //Пример сравнения строк по длине с помошью length
         return 1;
     }
-    if(str1.length < str2.length) {
+    if (str1.length < str2.length) {
         return -1;
     }
-    if(str1.length == str2.length) {
+    if (str1.length == str2.length) {
         return 0
     }
     throw "Unknown exception";
@@ -41,12 +60,60 @@ function firstCharToUpperElem(str1) {
 
 function vowelsCount(str) {
     let res = str.match(/[euioa]/ig); //Пример использования регулярного выражения и метода match() для подсчета гласных в строке
-    if(res != null){
+    if (res != null) {
         return res.length;
     }
     return 0;
 }
 
-function SpamDetector(str) {
-    
+function spamDetector(str) {
+    for (const word of spamWords) {
+        if (str.toUpperCase().includes(word.toUpperCase())) //Пример поиска определенной подстроки с помошью include()
+            return true;
+    }
+    return false;
+}
+
+function TruncateString(str, maxlength) {
+    if (str.length > maxlength) {
+        return str.substring(0, maxlength + 1)
+            .concat("...");
+    }
+    return str;
+}
+
+function isPalindrom(str) { //Пример функции, определяющей, является ли строка палиндромом.
+    //Вариант 1 - циклом. Если строка, которая является палиндромом, будет неравной по к-ву символов, например "ABCsCBA" - вернет true.
+
+    // for(let i = 0; i < str.length / 2; i++) {
+    //     if(str[i].toUpperCase() != str[str.length - (i + 1)].toUpperCase())
+    //         return false;
+    // }
+    // return true;
+
+    //Вариант 2 - с разделением строки на две части.
+    let firstPartOfTheString = str.slice(0, str.length / 2).toUpperCase();
+    let reversedsecondPartOfTheString = Array.from(
+        str.slice(str.length / 2).toUpperCase())
+        .reverse()
+        .join('');
+    if (firstPartOfTheString === reversedsecondPartOfTheString) //Пример сравнения строк с использования оператора строгого равно ("===").
+        return true;
+    return false;
+}
+
+function wordsCount(str) {
+    return str.split(" ").length;
+}
+
+function GetLongestWord(str) {
+    let strArray = str.split(" ");
+    strArray.sort(function (left, right) { //Пример сортировки массива по длине строчных элементов с помошью метода sort().
+        if(left.length < right.length)
+            return 1;
+        if(left.length > right.length)
+            return -1;
+        return 0;
+    });
+    return strArray[0];
 }
