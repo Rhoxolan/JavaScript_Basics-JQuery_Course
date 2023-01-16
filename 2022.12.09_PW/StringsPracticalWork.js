b87282a31807f7dd5d275d911d3ac7be004d592c
Rhoxolan <pba17mail@gmail.com>
2023-01-16 12:04:56 +0200

    The fifth practical work was supplemented

--- 2022.12.09_PW/StringsPracticalWork.js
+++ 2022.12.09_PW/StringsPracticalWork.js
@@ -11,6 +11,13 @@ console.log(getLongerString(str1, str3));
 console.log(str3);
 console.log(firstCharToUpperElem(str3));
 
+//Task 3.
+const string = "string";
+console.log(vowelsCount(str1));
+console.log(vowelsCount(string));
+console.log(vowelsCount("stroka"));
+console.log(vowelsCount("String Of Strings"));
+console.log(vowelsCount("String\nOf\nStrings"));
 
 
 function getLongerString(str1, str2) {
@@ -29,4 +36,13 @@ function getLongerString(str1, str2) {
 function firstCharToUpperElem(str1) {
     return str1[0].toUpperCase() + str1.slice(1); //Пример обрезки строки с помощью метода slice()
 }
+
+function vowelsCount(str) {
+    let res = str.match(/[euioa]/ig); //Пример использования регулярного выражения и метода match() для подсчета гласных в строке
+    if(res != null){
+        return res.length;
+    }
+    return 0;
+}
+
 
