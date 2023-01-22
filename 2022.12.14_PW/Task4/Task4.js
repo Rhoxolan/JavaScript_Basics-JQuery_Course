c623678fecd574bd449eb430848f3b2aa278d5d0
Rhoxolan <pba17mail@gmail.com>
2023-01-22 12:33:53 +0200

    The fourth task from the seventh practical work was finished

--- 2022.12.14_PW/Task4/Task4.js
+++ 2022.12.14_PW/Task4/Task4.js
@@ -1,16 +1,18 @@
-
+document.getElementById("JsDiv").onclick = textChenger;
+document.getElementById("HTMLDiv").onclick = textChenger;
+document.getElementById("CssDiv").onclick = textChenger;
 
 function textChenger(e) {
-    let textDiv = document.getElementById("textDiv");
-    switch (e.id) {
+    let textDiv = document.getElementById("textDiv"); 
+    switch (e.srcElement.id) { //Пример определения элемента, вызвавшего событие через srcElement.id
         case "JsDiv":
-            textDiv.innerText = "Lorem ipsum dolor sit amet JavaScript.";
+            textDiv.innerText = "Lorem ipsum dolor sit amet.";
             break;
         case "HTMLDiv":
-            textDiv.innerText = "Lorem ipsum dolor sit amet HTML.";
+            textDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing.";
             break;
         case "CssDiv":
-            textDiv.innerText = "Lorem ipsum dolor sit amet CSS.";
+            textDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus nisi at scelerisque molestie. Suspendisse.";
             break;
         default:
             break;
