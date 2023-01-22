8954661dcc5cb6d56803413ba313c89bea9234aa
Rhoxolan <pba17mail@gmail.com>
2023-01-22 23:17:24 +0200

    The seventh practical work was finished

--- 2022.12.14_PW/Task7/Task7.js
+++ 2022.12.14_PW/Task7/Task7.js
@@ -1,3 +1,4 @@
 //Пример поиска элементов на странице по селекторам элемента с помощью querySelectorAll()
 let tElems = document.querySelectorAll('td, th');
-tElems.forEach(e => e.onmousemove = e => e.srcElement.style["background-color"] = "red");
+tElems.forEach(e => e.onmouseover = e => e.srcElement.style["background-color"] = "#FFA984");
+tElems.forEach(e => e.onmouseout = e => e.srcElement.style["background-color"] = "");
