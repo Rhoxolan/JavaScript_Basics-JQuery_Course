d86c22f9fb5dabd3e623c61e1d644b875d976f46
Rhoxolan <pba17mail@gmail.com>
2023-01-25 22:31:43 +0200

    The first task from the nine practical work was finished

--- 2022.12.21_PW/Task1/Task1.js
+++ 2022.12.21_PW/Task1/Task1.js
@@ -1,1 +1,11 @@
-document.forms.loginForm //Попробовать добавить обраточик через добавить обработчик
+//Пример работы со списком форм document.forms; пример работы с событием "submit" формы
+document.forms.loginForm.addEventListener("submit", OnSubmit);
+
+function OnSubmit(e) {
+    if (document.forms.loginForm.elements.checkRememder.checked) { //Пример доступа к элементам input формы
+        alert(`Hi, ${e.currentTarget[0].value}! You are remembered!`) //Альтернативный способ доступа к элементам input формы - через параметр обработчика
+    }
+    else {
+        alert(`Hi, ${e.currentTarget[0].value}! You aren't remembered!`);
+    }
+}
