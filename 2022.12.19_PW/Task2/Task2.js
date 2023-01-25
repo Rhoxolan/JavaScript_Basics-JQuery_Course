2308d10e5915f4e7df6a714f097209bbdff811a1
Rhoxolan <pba17mail@gmail.com>
2023-01-25 09:39:15 +0200

    The second task from the eight practical work was finished

--- 2022.12.19_PW/Task2/Task2.js
+++ 2022.12.19_PW/Task2/Task2.js
@@ -2,5 +2,11 @@ const divsDiv = document.getElementById("divsDiv");
 
 document.getElementById("addDivButton").onclick = e => {
     divsDiv.appendChild(document.createElement("div")); //Пример добавления элементов с использованием document.createElement()
-    divsDiv.lastChild.className = "coloredDiv"; //Пример обращения к элементу-lastChild
+    const lastChild = divsDiv.lastChild;
+    lastChild.className = "coloredDiv"; //Пример обращения к элементу-lastChild
+    lastChild.style.backgroundColor = `rgb(
+        ${Math.round(Math.random() * 255)},
+        ${Math.round(Math.random() * 255)},
+        ${Math.round(Math.random() * 255)})`;
+    lastChild.onclick = e => lastChild.remove(); //Пример удаления элемента с использованием remove
 };
