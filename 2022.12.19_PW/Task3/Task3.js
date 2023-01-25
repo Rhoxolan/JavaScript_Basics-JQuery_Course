1e7d027d7ac82310bb944306e42d6773bc1d6fb4
Rhoxolan <pba17mail@gmail.com>
2023-01-25 12:18:15 +0200

    The third task from the eight practical work was finished

--- 2022.12.19_PW/Task3/Task3.js
+++ 2022.12.19_PW/Task3/Task3.js
@@ -1,3 +1,3 @@
 const textP = document.getElementById("textP");
-
-document.querySelectorAll(".cell").forEach(c => c.onclick = );
+document.querySelectorAll(".cell").forEach(c => c.onclick = e =>
+    textP.style.color = e.srcElement.style.backgroundColor);
