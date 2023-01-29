f813691e4b6601b72b7aab521878e78380e3e1ac
Rhoxolan <pba17mail@gmail.com>
2023-01-29 10:49:22 +0200

    The tenth practical work was supplemented

--- 2022.12.23_PW/userInfo.js
+++ 2022.12.23_PW/userInfo.js
@@ -2,12 +2,44 @@ function onLoad() {
     document.cookie.split('; ').forEach(c => {
         if (c.includes("email"))
             document.getElementById("eMailSpan").innerText = c.split('=')[1];
-            return;
+        return;
     });
 }
 
 document.getElementById("exitButton").addEventListener("click", onExitButtonClick);
 
 function onExitButtonClick(e) {
+    deleteCookies();
+    location.href = "/registrationIndex.html";
+}
+
+function deleteCookies() {
+    let currentCookies = document.cookie.split('; ');
+    currentCookies.forEach(c => deleteCookie(c.split('=')[0]));
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", {'max-age': -1});
+}
+
+function setCookie(name, value, options = {}) {
+    let updatedCookie = name + "=" + value;
     
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function newDeleteCookies() {
+    let updatedCookies = new String();
+    document.cookie.split('; ').forEach(c => {
+        updatedCookies += c + ";max-age=-1";
+    });
+    document.cookie = updatedCookies;
 }
