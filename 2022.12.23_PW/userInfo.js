ec4fee35200a267dbb134db6332b62047d184f58
Rhoxolan <pba17mail@gmail.com>
2023-01-29 18:34:23 +0200

    The tenth practical work was supplemented

--- 2022.12.23_PW/userInfo.js
+++ 2022.12.23_PW/userInfo.js
@@ -7,8 +7,14 @@ function onLoad() {
 }
 
 document.getElementById("exitButton").addEventListener("click", onExitButtonClick);
+document.forms.userInfoFrom.addEventListener("submit", onSubmit);
 
 function onExitButtonClick(e) {
     document.cookie.split('; ').forEach(c => document.cookie = `${c}; max-age=-1`); //Пример алгоритма по удалению всех coockies c document.cookie
     location.replace("/registrationIndex.html");
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+    let checker = false;
 }
