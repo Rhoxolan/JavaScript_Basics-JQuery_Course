9490f95e2722e42701075485f143fdb4752e2af5
Rhoxolan <pba17mail@gmail.com>
2023-01-30 10:05:16 +0200

    The tenth practical work was finished

--- 2022.12.23_PW/userInfo.js
+++ 2022.12.23_PW/userInfo.js
@@ -1,8 +1,42 @@
 function onLoad() {
-    document.cookie.split('; ').forEach(c => {
-        if (c.includes("email"))
+    let checker = false;
+    let cookies = document.cookie.split('; ');
+    for (const c of cookies) {
+        if (c.includes("email")) {
             document.getElementById("eMailSpan").innerText = c.split('=')[1];
+            checker = true;
+            break;
+        }
+    }
+    if (!checker) {
+        location.replace("/registrationIndex.html");
         return;
+    }
+    cookies.forEach(c => {
+        if (c.split('=')[0] == "firstName") {
+            document.forms.userInfoFrom.elements.firstNameInput.value = c.split('=')[1];
+            return; //return здесь остагавливает текущую итерацию
+        }
+        if (c.split('=')[0] == "lastName") {
+            document.forms.userInfoFrom.elements.lastNameInput.value = c.split('=')[1];
+            return;
+        }
+        if (c.split('=')[0] == "yearOfBirth") {
+            document.forms.userInfoFrom.elements.yearOfBirthInput.value = c.split('=')[1];
+            return;
+        }
+        if (c.split('=')[0] == "phoneNumber") {
+            document.forms.userInfoFrom.elements.phoneNumberInput.value = c.split('=')[1];
+            return;
+        }
+        if (c.split('=')[0] == "skype") {
+            document.forms.userInfoFrom.elements.skypeInput.value = c.split('=')[1];
+            return;
+        }
+        if (c.split('=')[0] == "gender") {
+            document.forms.userInfoFrom.elements.genderInput.value = c.split('=')[1];
+            return;
+        }
     });
 }
 
@@ -53,10 +87,24 @@ function onSubmit(e) {
         }
     }
     else {
-        //Удалить кук телефона
+        document.cookie = "phoneNumber=; max-age=-1";
+    }
+    if (document.forms.userInfoFrom.elements.skypeInput.value != "") {
+        if (document.forms.userInfoFrom.elements.skypeInput.value.length < 50) {
+            document.getElementById("wrongSkypeDiv").style.visibility = "hidden";
+            document.cookie = `skype=${document.forms.userInfoFrom.elements.skypeInput.value}; max-age=3600`;
+        }
+        else {
+            document.getElementById("wrongSkypeDiv").style.visibility = "visible";
+            checker = true;
+        }
+    }
+    else {
+        document.cookie = "skype=; max-age=-1";
     }
     if (checker) {
         return false;
     }
     document.cookie = `gender=${document.forms.userInfoFrom.elements.genderInput.value}; max-age=3600`;
+    alert("Data is updated");
 }
