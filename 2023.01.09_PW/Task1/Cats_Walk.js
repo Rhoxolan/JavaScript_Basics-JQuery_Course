b5de910da9b837fc97f8b128bacd1b0dd91e30d3
Rhoxolan <pba17mail@gmail.com>
2023-02-02 21:55:28 +0200

    The first task from the twelfth practical work was finished

--- 2023.01.09_PW/Task1/Cats_Walk.js
+++ 2023.01.09_PW/Task1/Cats_Walk.js
@@ -1,4 +1,4 @@
-$('body').mousemove(function(e) {
+$('body').mousemove(function(e) { //Пример работы с событием (с использованием спец.метода для обработки событий) в jQuery
     console.log(e.pageX);
     $("#catDiv").css('top', `${e.pageY - 150}px`); //Пример выборки элемента по селектору id в jQuery; Пример установки стиля css элементу с помощью jQuery 
     $("#catDiv").css('left', `${e.pageX - 150}px`);
