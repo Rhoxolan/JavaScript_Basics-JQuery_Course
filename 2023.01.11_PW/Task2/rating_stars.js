aef2b2514f20a22b67dd2609599f9c65ac6e4fcd
Rhoxolan <pba17mail@gmail.com>
2023-02-04 12:17:41 +0200

    The second part from the thirteenth practical work was supplemented

--- 2023.01.11_PW/Task2/rating_stars.js
+++ 2023.01.11_PW/Task2/rating_stars.js
@@ -1,5 +1,5 @@
-$('div').on('click', '.starDiv', function () {
+$(".starDiv").click((e) => {
     var set = $('.starDiv');
-    var index = set.index(this);
-    
+    var index = set.index($(e.target)); //Пример определения индекса элемента с помошью метода index() в jQuery
+    $(e.target).nextAll().css("background-image", "url(/Task2/Stars/black_star.png)");
 });
