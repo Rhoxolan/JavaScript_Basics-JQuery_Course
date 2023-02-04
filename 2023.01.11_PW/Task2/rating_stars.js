ad9eb2cf03ae4091a4d70529537d0faa31aba1db
Rhoxolan <pba17mail@gmail.com>
2023-02-04 18:44:45 +0200

    The second part from the thirteenth practical work was finished

--- 2023.01.11_PW/Task2/rating_stars.js
+++ 2023.01.11_PW/Task2/rating_stars.js
@@ -1,5 +1,7 @@
 $(".starDiv").click((e) => {
     var set = $('.starDiv');
     var index = set.index($(e.target)); //Пример определения индекса элемента с помошью метода index() в jQuery
-    $(e.target).nextAll().css("background-image", "url(/Task2/Stars/black_star.png)");
+    $(e.target).nextAll().css("background-image", "url(/Task2/Stars/black_star.png)"); //Пример нахождения сестринских элементов с помошью методов серии next и prev в jQuery
+    $(e.target).css("background-image", "url(/Task2/Stars/gold_star.png)");
+    $(e.target).prevUntil("h2").css("background-image", "url(/Task2/Stars/gold_star.png)");
 });
